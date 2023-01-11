0b8fa0c50754815b6cc2b3f34acb27822d596344
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2023-01-11 16:04:00 +0600

    adds test for generate percent in spelling

---
+++ tests/Spellings/PercentWriteSpellingTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ZhandosProg\WriteSpelling\Spellings;
+
+use PHPUnit\Framework\TestCase;
+use ZhandosProg\WriteSpelling\Exceptions\NotSupportedException;
+use ZhandosProg\WriteSpelling\Exceptions\ValidationException;
+
+class PercentWriteSpellingTest extends TestCase
+{
+
+    public function testGenerateRu(): void
+    {
+        $expected = 'сорок две целых сорок две сотых процента';
+        $actual = (new PercentWriteSpelling())->generate(42.42,'ru');
+        $this->assertEquals($expected, $actual);
+
+        $expected = 'сорок два процента';
+        $actual = (new PercentWriteSpelling())->generate(42,'ru');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGenerateKz(): void
+    {
+        $expected = 'қырық екі бүтін жүзден қырық екі пайыз';
+        $actual = (new PercentWriteSpelling())->generate(42.42);
+        $this->assertEquals($expected, $actual);
+
+        $expected = 'қырық екі пайыз';
+        $actual = (new PercentWriteSpelling())->generate(42);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGenerateNumberFormatValidationException(): void
+    {
+        $amountWriteSpelling = new PercentWriteSpelling();
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Incorrect number format!');
+
+        $amountWriteSpelling->generate('1asd');
+    }
+
+    public function testGenerateRangePercentValidationException(): void
+    {
+        $amountWriteSpelling = new PercentWriteSpelling();
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('The percentage must have a range of 0 to 100!');
+
+        $amountWriteSpelling->generate('101');
+    }
+
+    public function testGenerateNotSupportedException(): void
+    {
+        $locale = 'en';
+        $amountWriteSpelling = new PercentWriteSpelling();
+
+        $this->expectException(NotSupportedException::class);
+        $this->expectErrorMessage('Localization '. strtoupper($locale) .' not supported!');
+
+        $amountWriteSpelling->generate(12, $locale);
+    }
+}
