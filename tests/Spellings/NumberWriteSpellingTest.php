5b2256bae95cb069e7e8858f26033d3f79174f9c
Zhandos-prog <sutemgenov.zhandos@gmail.com>
2023-09-21 08:51:27 +0600

    adds tests for number generate module

---
+++ tests/Spellings/NumberWriteSpellingTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ZhandosProg\WriteSpelling\Tests\Spellings;
+
+use ZhandosProg\WriteSpelling\Exceptions\NotSupportedException;
+use ZhandosProg\WriteSpelling\Exceptions\ValidationException;
+use ZhandosProg\WriteSpelling\Spellings\NumberWriteSpelling;
+use PHPUnit\Framework\TestCase;
+
+class NumberWriteSpellingTest extends TestCase
+{
+    public function testGenerateRu(): void
+    {
+        $expected = 'двенадцать целых девяносто девять сотых';
+        $actual = (new NumberWriteSpelling())->generate(12.99,'ru');
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGenerateKz(): void
+    {
+        $expected = 'он екі бүтін тоқсан тоғыз жүзден';
+        $actual = (new NumberWriteSpelling())->generate(12.99);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testGenerateValidationException(): void
+    {
+        $amountWriteSpelling = new NumberWriteSpelling();
+
+        $this->expectException(ValidationException::class);
+        $this->expectErrorMessage('Incorrect number format!');
+
+        $amountWriteSpelling->generate('1asd');
+    }
+
+    public function testGenerateNotSupportedException(): void
+    {
+        $locale = 'en';
+        $amountWriteSpelling = new NumberWriteSpelling();
+
+        $this->expectException(NotSupportedException::class);
+        $this->expectErrorMessage('Localization '. strtoupper($locale) .' not supported!');
+
+        $amountWriteSpelling->generate(12, $locale);
+    }
+}
